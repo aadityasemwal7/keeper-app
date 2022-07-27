c3e7dc8de9e20b018c5cc530984f5efb0892b131
Aaditya Semwal <93855836+aadityasemwal7@users.noreply.github.com>
2022-07-28 02:04:31 +0530

    Updates

---
+++ precache-manifest.9889a8e9c252ee0232d635be94d039a2.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a4a05f6067aaa79d015117b4b8c8c68c",
+    "url": "/keeper-app/index.html"
+  },
+  {
+    "revision": "4530b8c5089b9f78e901",
+    "url": "/keeper-app/static/js/2.6a20179d.chunk.js"
+  },
+  {
+    "revision": "87eff4b9dda2f676048c",
+    "url": "/keeper-app/static/js/main.dafff46a.chunk.js"
+  },
+  {
+    "revision": "3f5fcd70531cb0f5d766",
+    "url": "/keeper-app/static/js/runtime-main.ad8fc91b.js"
+  }
+]);
